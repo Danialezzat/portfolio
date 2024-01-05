a64ed22de79dba454b1370e624c53016032deb58
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-05 03:43:56 -0800

    DONE WITH WORK COMPONENT

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import WorkImg from '../assets/projects/workImg.jpeg';
-import realState from '../assets/projects/realestate.jpg';
+import Banking from '../assets/projects/hoobank.png';
+import Nike from '../assets/projects/nike.png';
+import Nexcent from '../assets/projects/nexcent.png';
+import Advice from '../assets/projects/advice.png';
+import Finance from '../assets/projects/finance.png';
+import CRUD from '../assets/projects/crud.png';
 
 const Work = () => {
   return (
@@ -17,7 +21,7 @@ const Work = () => {
 
                 {/* grid item */}
                 <div 
-                    style={{ backgroundImage: `url(${WorkImg})`}}
+                    style={{ backgroundImage: `url(${Nexcent})`}}
                     className='shadow-lg shadow-[#040c16] group container  rounded-md flex justify-center items-center mx-auto  content-div '>
 
 
@@ -27,10 +31,10 @@ const Work = () => {
 React JS Application
                         </span>
                         <div className='pt-8 text-center '>
-                            <a href="/">
+                            <a href="https://dreamy-lily-5a3834.netlify.app/">
                                 <button className='text-center rounded-l-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href="/">
+                            <a href="https://github.com/Danialezzat/Nextcent">
                                 <button className='text-center rounded-r-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
@@ -38,7 +42,7 @@ React JS Application
                 </div>
                 {/* grid item */}
                 <div 
-                    style={{ backgroundImage: `url(${WorkImg})`}}
+                    style={{ backgroundImage: `url(${Banking})`}}
                     className='shadow-lg shadow-[#040c16] group container  rounded-md flex justify-center items-center mx-auto  content-div '>
 
 
@@ -48,10 +52,10 @@ React JS Application
 React JS Application
                         </span>
                         <div className='pt-8 text-center '>
-                            <a href="/">
+                            <a href="https://tubular-pika-a9805b.netlify.app">
                                 <button className='text-center rounded-l-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href="/">
+                            <a href="https://github.com/Danialezzat/Hoobank-mini-project">
                                 <button className='text-center rounded-r-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
@@ -59,7 +63,7 @@ React JS Application
                 </div>
                 {/* grid item */}
                 <div 
-                    style={{ backgroundImage: `url(${WorkImg})`}}
+                    style={{ backgroundImage: `url(${Advice})`}}
                     className='shadow-lg shadow-[#040c16] group container  rounded-md flex justify-center items-center mx-auto  content-div '>
 
 
@@ -69,10 +73,10 @@ React JS Application
 React JS Application
                         </span>
                         <div className='pt-8 text-center '>
-                            <a href="/">
+                            <a href="https://peaceful-fudge-743d80.netlify.app">
                                 <button className='text-center rounded-l-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href="/">
+                            <a href="https://github.com/Danialezzat/advice">
                                 <button className='text-center rounded-r-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
@@ -80,7 +84,7 @@ React JS Application
                 </div>
                 {/* grid item */}
                 <div 
-                    style={{ backgroundImage: `url(${WorkImg})`}}
+                    style={{ backgroundImage: `url(${Finance})`}}
                     className='shadow-lg shadow-[#040c16] group container  rounded-md flex justify-center items-center mx-auto  content-div '>
 
 
@@ -93,7 +97,7 @@ React JS Application
                             <a href="/">
                                 <button className='text-center rounded-l-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href="/">
+                            <a href="https://github.com/Danialezzat/finence">
                                 <button className='text-center rounded-r-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
@@ -101,7 +105,7 @@ React JS Application
                 </div>
                 {/* grid item */}
                 <div 
-                    style={{ backgroundImage: `url(${WorkImg})`}}
+                    style={{ backgroundImage: `url(${Nike})`}}
                     className='shadow-lg shadow-[#040c16] group container  rounded-md flex justify-center items-center mx-auto  content-div '>
 
 
@@ -111,18 +115,19 @@ React JS Application
 React JS Application
                         </span>
                         <div className='pt-8 text-center '>
-                            <a href="/">
+                            <a href="https://dazzling-melomakarona-560b63.netlify.app">
                                 <button className='text-center rounded-l-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href="/">
+                            <a href="https://github.com/Danialezzat/nikeMiniProject">
                                 <button className='text-center rounded-r-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
                     </div>
                 </div>
+
                 {/* grid item */}
                 <div 
-                    style={{ backgroundImage: `url(${WorkImg})`}}
+                    style={{ backgroundImage: `url(${CRUD})`}}
                     className='shadow-lg shadow-[#040c16] group container  rounded-md flex justify-center items-center mx-auto  content-div '>
 
 
@@ -132,36 +137,16 @@ React JS Application
 React JS Application
                         </span>
                         <div className='pt-8 text-center '>
-                            <a href="/">
+                            <a href="https://dulcet-kheer-825472.netlify.app">
                                 <button className='text-center rounded-l-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href="/">
+                            <a href="https://github.com/Danialezzat/CRUD-project">
                                 <button className='text-center rounded-r-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
                     </div>
                 </div>
-                {/* grid item */}
-                <div 
-                    style={{ backgroundImage: `url(${WorkImg})`}}
-                    className='shadow-lg shadow-[#040c16] group container  rounded-md flex justify-center items-center mx-auto  content-div '>
-
-
-                    {/* hover effect */}
-                    <div className='opacity-0 group-hover:opacity-100 w-full hover:backdrop-blur-3xl flex flex-col justify-center items-center h-[100%] duration-500'>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-React JS Application
-                        </span>
-                        <div className='pt-8 text-center '>
-                            <a href="/">
-                                <button className='text-center rounded-l-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href="/">
-                                <button className='text-center rounded-r-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                
 
 
 
