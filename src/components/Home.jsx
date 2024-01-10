4bfdfb3f27b16265212755eee0ee2496bdf1f8cd
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-10 14:19:07 -0800

    home text colors changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,14 +8,14 @@ const Home = ({isDarkMode}) => {
 
         {/* container */}
         <div className='max-w-[1000px] mx-auto px-10 flex flex-col justify-center h-full'>
-            <p className='text-pink-600 font-bold text-xl'>Hi, my name is</p>
-            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Danial Ezzatmadar</h1>
+            <p className='text-[#9896f1] font-bold text-xl'>Hi, my name is</p>'text-[#9896f1]'
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#9896f1]'>Danial Ezzatmadar</h1>
             <h2 className='text-4xl sm:texl-7xl font-bold text-[#8892b0]'>I'm a junior Frontend web Developer</h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>my about</p>
 
             <div>
                 
-                <button className={`${isDarkMode ? 'font-black' : 'text-white'}  group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600`}>
+                <button className={`${isDarkMode ? 'font-black' : 'text-white'}  group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#9896f1] hover:border-[#9896f1]`}>
                 <Link to="skills" smooth={true} duration={500}>
                 view work
                 </Link>
