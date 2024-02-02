6c890b5f58719d7259c5ef8a2af4aa1b3121787d
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-02-02 12:07:29 -0800

    color of burger is changed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,12 +9,12 @@ const Contact = ({isDarkMode}) => {
         <form method='POST' action="https://getform.io/f/dabafa22-8310-4e87-a6fe-cf1b0cef4fc1" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8 '>
                 <p className={`${isDarkMode ? 'text-[#9896f1]' : '' } text-4xl font-bold inline border-b-4 border-pink-600`}>Contact</p>
-                <p className={`${isDarkMode ? 'text-black' : 'text-gray-300'}  py-4`}>Submit the form below or shoot me an email</p>
+                <p className={`${isDarkMode ? 'text-black' : 'text-gray-300'} font-semibold  py-4`}>Submit the form below or shoot me an email</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]'  type="email" placeholder='Email' name='email' />
             <textarea className='bg-[#ccd6f6] p-2' name="message" id=""  rows="10" placeholder='Message'></textarea>
-            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 flex justify-center items-center'>Let's Collaborate</button>
+            <button className='text-black font-semibold border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 flex justify-center items-center'>Let's Collaborate</button>
         </form>
 
           <div className='w-full  grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4 text-center py-8'>
