831ed83b2cfd19aa26e8a2b59867ef0104a9fb92
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-02-03 13:16:54 -0800

    states for the data is made

--- src/blog/pages/CreatePost.js
+++ src/blog/pages/CreatePost.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("")
+  const [postText, setPostText] = useState("")
+
+
+
   return (
     <div>
       <div>
         <h1>Create a Post</h1>
         <div className='inputGroup'>
           <label htmlFor="title">Title</label>
-          <input id='title' type="text" placeholder='Title...' />
+          <input id='title' type="text" placeholder='Title...' onChange={(event) => {setTitle(event.target.value)}} />
         </div>
         <div className='inputGroup'>
         <label htmlFor="post">post</label>
-        <textarea name="" id="post" placeholder='Write here...' cols="30" rows="10"></textarea>
+        <textarea name="" id="post" placeholder='Write here...' onChange={(event) => {setPostText(event.target.value)}} ></textarea>
         </div>
         <button>Submit post</button>
       </div>
