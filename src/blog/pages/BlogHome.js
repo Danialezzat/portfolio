e5d547529468d0588e58e14a5cb4b034593e5c30
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-02-03 11:57:26 -0800

    linkes of blog components are made

--- src/blog/pages/BlogHome.js
+++ src/blog/pages/BlogHome.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Outlet,Link } from 'react-router-dom';
 
 const BlogHome = () => {
   return (
-    <div>BlogHome</div>
+    <div>
+      <nav>
+        <Link to='/bloghome' >Home</Link>
+        <Link to='/bloghome/createpost' >Create Post</Link>
+        <Link to='/bloghome/bliglogin' >Login</Link>
+        <Link to='/portfolio' >Portfolio</Link>
+      </nav>
+      <h1>
+        BlogHome  
+      </h1>
+
+      <Outlet />
+      
+    </div>
   )
 }
 
