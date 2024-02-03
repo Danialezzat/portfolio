fb34305aa38dade062b6f09dcefbf6bf1c991d1d
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-02-03 12:22:31 -0800

    log in button got functional

--- src/blog/pages/BlogLogin.js
+++ src/blog/pages/BlogLogin.js
@@ -6,13 +6,14 @@ const BlogLogin = ({ setIsAuth }) => {
 
   const signInWithGoogle = () => {
     signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true)
       setIsAuth(true)
     })
   }
   return (
     <div>
       <p>sign in with google to continue</p>
-      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign In with Google</button>
+      <button onClick={signInWithGoogle} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign In with Google</button>
     </div>
   )
 }
