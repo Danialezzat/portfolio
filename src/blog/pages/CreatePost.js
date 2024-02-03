9bd117bb5f1ce16729b2a95353e31bec4c89b955
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-02-03 13:12:56 -0800

    create post page primary tags are made

--- src/blog/pages/CreatePost.js
+++ src/blog/pages/CreatePost.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div>
+      <div>
+        <h1>Create a Post</h1>
+        <div className='inputGroup'>
+          <label htmlFor="title">Title</label>
+          <input id='title' type="text" placeholder='Title...' />
+        </div>
+        <div className='inputGroup'>
+        <label htmlFor="post">post</label>
+        <textarea name="" id="post" placeholder='Write here...' cols="30" rows="10"></textarea>
+        </div>
+        <button>Submit post</button>
+      </div>
+    </div>
   )
 }
 
