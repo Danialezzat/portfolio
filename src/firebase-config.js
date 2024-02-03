a522b25024cac5fb4f5b20ccea1f6a3be248983a
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-02-03 10:00:48 -0800

    firebase is unstlled and the firebase file is made

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBR9JOMqy_BI07vkF4ADYDXnBjA-txpO8k",
+  authDomain: "my-blog-125e8.firebaseapp.com",
+  projectId: "my-blog-125e8",
+  storageBucket: "my-blog-125e8.appspot.com",
+  messagingSenderId: "542435114402",
+  appId: "1:542435114402:web:62e5ea36fe14eae16cc6fb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
