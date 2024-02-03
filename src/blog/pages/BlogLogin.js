0c93a45f1bd74821199c4e899b917a0e991a9f91
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-02-03 12:26:48 -0800

    usenavigate is added for the login page

--- src/blog/pages/BlogLogin.js
+++ src/blog/pages/BlogLogin.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { auth, provider } from '../../firebase-config';
-import {signInWithPopup} from 'firebase/auth'
+import {signInWithPopup} from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 const BlogLogin = ({ setIsAuth }) => {
+  let navigate = useNavigate();
 
   const signInWithGoogle = () => {
     signInWithPopup(auth, provider).then((result) => {
-      localStorage.setItem("isAuth", true)
-      setIsAuth(true)
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate('/bloghome')
     })
   }
   return (
