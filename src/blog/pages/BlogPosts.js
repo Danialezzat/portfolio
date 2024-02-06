f568d5374a661553cb776089342015c6f380fcb2
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-02-06 02:32:07 -0800

    deleting iscon is fixed

--- src/blog/pages/BlogPosts.js
+++ src/blog/pages/BlogPosts.js
@@ -1,6 +1,6 @@
-import { collection, deleteDoc, getDocs, doc } from 'firebase/firestore';
+import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore'; // deleteDoc, doc
 import React, { useEffect, useState } from 'react'
-import { auth, db } from '../../firebase-config';
+import { auth, db } from '../../firebase-config'; //auth
 
 const BlogPosts = ({isAuth, setIsAuth}) => {
   const [postLists, setPostList] = useState([]);
@@ -16,8 +16,8 @@ const BlogPosts = ({isAuth, setIsAuth}) => {
   },[]);
 
   const deletePost = async (id) => {
-    const postDoc = doc(db, "post", id)
-    await deleteDoc(postDoc)
+    const postDoc = doc(db, "post", id);
+    await deleteDoc(postDoc);
   }
 
 
@@ -25,18 +25,18 @@ const BlogPosts = ({isAuth, setIsAuth}) => {
     <div>
             {postLists.map((post) => {
               return(
-              <div className='text-black font-semi'>
+              <div key={post.id} className='text-black font-semi'>
                             <div className='post-header'>
                                 <div className='title'>
                                     <h1>{post.title}</h1>
                                 </div>
                                 <div className='deltepost'>
-                                    {isAuth && post.author.id === auth.currentUser.uid && <button onClick={deletePost(post.id)} >&#128465;</button>}
+                                    {isAuth && post.author.id === auth.currentUser.uid && <button onClick={() => deletePost(post.id)} >&#128465;</button>}
                                 </div>
                             </div>
                             <div className='postTextContainer'>{post.postText}</div>
                             <h3>@{post.author.name}</h3>
-                    </div>)
+              </div>)
             })}
     </div>
   )
