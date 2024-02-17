6327056afe4b96459fad852455144d9db355b04f
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-02-17 14:04:07 -0800

    text are more orgenized

--- src/blog/pages/BlogPosts.js
+++ src/blog/pages/BlogPosts.js
@@ -20,7 +20,9 @@ const BlogPosts = ({ isAuth, setIsAuth, isDarkMode }) => {
   const postsCollectionRef = collection(db, "post");
 
   const getPosts = async () => {
-    const data = await getDocs(query(postsCollectionRef, orderBy("createdAt", "desc")));
+    const data = await getDocs(
+      query(postsCollectionRef, orderBy("createdAt", "desc"))
+    );
     setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
   };
 
@@ -78,8 +80,8 @@ const BlogPosts = ({ isAuth, setIsAuth, isDarkMode }) => {
             <h1 className="text-2xl text-left bg-white bg-opacity-80 w-full h-[50px] font-bold p-2 absolute top-[254px]">
               {post.title}
             </h1>
-            <div className=" h-[180px] overflow-y-auto">
-              <p className="font-semibold  p-2 text-justify w-full">
+            <div className=" h-[80px] overflow-y-auto pb-8">
+              <p className="font-semibold  p-2  text-justify w-full ">
                 {post.postText}
               </p>
             </div>
@@ -106,7 +108,7 @@ const BlogPosts = ({ isAuth, setIsAuth, isDarkMode }) => {
                   likePost(post.id);
                 }
               }}
-              className="absolute left-2 bottom-8 text-xl cursor-pointer p-1"
+              className="absolute left-2 bottom-8 text-xl cursor-pointer p-1 bg-white"
             >
               {isAuth &&
                 (post.liked.indexOf(auth.currentUser.uid) !== -1 ? (
@@ -115,9 +117,11 @@ const BlogPosts = ({ isAuth, setIsAuth, isDarkMode }) => {
                   <FaRegHeart />
                 ))}
             </div>
-            <p className="font-bold absolute left-2 bottom-2 text-xl cursor-pointer p-1">
-              {post.likes} Likes
-            </p>
+            <dir>
+              <p className="font-bold absolute left-2 bottom-2 text-xl cursor-pointer p-1">
+                {post.likes} Likes
+              </p>
+            </dir>
           </div>
         );
       })}
