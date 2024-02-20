203f820711997f4e0d1c5c3eecac717f2c2b7d8f
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-02-20 13:59:29 -0800

    changed button's type

--- src/blog/pages/BlogLogin.js
+++ src/blog/pages/BlogLogin.js
@@ -93,6 +93,7 @@ const BlogLogin = () => {
             </button>
               <div className="w-[48%] flex items-center justify-center h-screen dark:bg-gray-800">
                 <button
+                type="button"
                  onClick={signInWithGoogle} className="px-4 py-2 border flex justify-center items-center gap-2 bg-white border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150 h-[60px] ">
                     <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" />
                     <span>Login with Google</span>
